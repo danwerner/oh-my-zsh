b1e5f17275b86a990b7518c4e93e342c7a52f960
Daniel Werner <daniel.werner@fineway.de>
2016-02-11 09:27:40 +0100

    virtualenvwrapper plugin: Use custem $VIRTUALENVWRAPPER_SCRIPT variable

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.zsh
@@ -6,6 +6,13 @@ if (( $+commands[$virtualenvwrapper] )); then
     unsetopt equals
     source ${${virtualenvwrapper}:c}
   }
+elif [[ -f "$VIRTUALENVWRAPPER_SCRIPT" ]]; then
+  function {
+    setopt local_options
+    unsetopt equals
+    virtualenvwrapper="$VIRTUALENVWRAPPER_SCRIPT"
+    source "$VIRTUALENVWRAPPER_SCRIPT"
+  }
 elif [[ -f "/etc/bash_completion.d/virtualenvwrapper" ]]; then
   function {
     setopt local_options
